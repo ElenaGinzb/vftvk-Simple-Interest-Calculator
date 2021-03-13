3e7a8e545ddf2c8db2a906761734ba69eac82ddf
Elena <e_lginz@msn.com>
2021-03-13 17:15:27 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
     
+    result = ((p/100)*r)*y;
+    var d = new Date();
+    var year= d.getFullYear();
+    term = parseInt(y,10) + year;
+    document.getElementById("total").innerHTML = "If you deposit " + p + ",</br>" +  " at an interest rate of " + r + ".</br> "  + "You will receive an amount " + result + ",</br> "  + " in the year " + term;
 }
+
         
